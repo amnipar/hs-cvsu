45222dd418dd83253cb38a86ed1d67e21dd288be
Matti Johannes Eskelinen <matti.j.eskelinen@gmail.com>
2013-01-21 11:54:36 +0200

    require specifying format in opencv conversions

--- CVSU/OpenCV.hs
+++ CVSU/OpenCV.hs
@@ -1,4 +1,4 @@
-module CVSU.OpenCV 
+module CVSU.OpenCV
 ( C'IplImage(..)
 , fromIplImage
 , toIplImage
@@ -13,23 +13,25 @@ import Foreign.ForeignPtr hiding (newForeignPtr)
 import Foreign.Storable
 import Foreign.Marshal.Alloc
 
-fromIplImage :: Ptr () -> IO (PixelImage)
-fromIplImage ptr = do
+
+
+fromIplImage :: PixelFormat -> Ptr () -> IO (PixelImage)
+fromIplImage format ptr = do
   fimg <- allocPixelImage
   withForeignPtr fimg $ \pimg -> do
-    result <- c'pixel_image_create_from_ipl_image pimg (castPtr ptr) c'GREY
+    result <- c'pixel_image_create_from_ipl_image pimg (castPtr ptr) (cPixelFormat format)
     if result /= c'SUCCESS
        then error $ "fromIplImage failed with " ++ (show result)
        else ptrToPixelImage fimg
 
-toIplImage :: PixelImage -> IO (Ptr (C'IplImage))
-toIplImage img = do
+toIplImage :: PixelFormat -> PixelImage -> IO (Ptr (C'IplImage))
+toIplImage format img = do
   withForeignPtr (imagePtr img) $ \pimg -> do
     let
       mallocIplImage :: IO (Ptr (Ptr C'IplImage))
       mallocIplImage = malloc
     ppipl <- mallocIplImage
-    r <- c'ipl_image_create_from_pixel_image ppipl pimg c'RGB
+    r <- c'ipl_image_create_from_pixel_image ppipl pimg (cPixelFormat format)
     if r /= c'SUCCESS
        then error "Creating IplImage failed"
        else do
