791efa9f55d014ea40e2fded3ca7ed2516da7e0f
Matti Johannes Eskelinen <matti.j.eskelinen@gmail.com>
2013-02-07 16:53:47 +0200

    removed unnecessary files from examples

--- examples/test-pnm.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import CVSU.PixelImage
-import ReadArgs
-
-main = do
-  (sourceFile, targetFile, ascii) <- readArgs
-  ppm <- readPNMPixelImage sourceFile
-  writePNMPixelImage targetFile (ascii == "1") ppm
